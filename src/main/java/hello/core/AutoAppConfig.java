16e40a7307a71633193026e26f3f0934d47f210c
DESKTOP-QBRQJT0\USER <6545742@naver.com>
2022-12-29 23:30:19 +0900

    탐색 위치 지정

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.FilterType;
 @Configuration
 //@Component가 붙은 것을 다 스캔해서 스프링 빈으로 등록해줌
 @ComponentScan (
+        basePackages = "hello.core", //스캔할 패키지를 지정 (지정하지 않으면 @ComponentScan이 붙은 클래스의 패키지가 시작 위치가 됨)
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
         //Configuration도 스캔하기 떄문에 이건 뺴줌
 )
